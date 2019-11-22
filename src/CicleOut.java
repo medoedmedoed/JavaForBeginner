fed836da1a21d4fa83c70dfffedd992918147fac
Kirill <khromov.kirill@yandex.ru>
2019-11-22 23:31:19 +0300

    Задача №8: Цикличный вывод данных на экран

---
+++ src/CicleOut.java
@@ -0,0 +1,18 @@
+/*
+Задача №8: Цикличный вывод данных на экран
+Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+*/
+public class CicleOut {
+    public static void main(String[] args) {
+        /*for(int i = 0; i < 50 ; ++i){
+            System.out.println(i+1);
+        }*/
+        int i = 0;
+        /*while(++i < 51)
+            System.out.println(i);*/
+        do{
+            ++i;
+            System.out.println(i);
+        }while(++i < 51);
+    }
+}
